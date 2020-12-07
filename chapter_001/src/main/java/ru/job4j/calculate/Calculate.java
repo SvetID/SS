ee3fffd99c8725bd8340a079e16f28627f266e0a
Svetlana Dubrova <svetd24@gmail.com>
2020-12-07 21:30:18 +0500

    add new module chapter 001 add javadoc plugin

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,11 @@
 package ru.job4j.calculate;
 
+/**
+ * @author SvetD
+ */
 public class Calculate {
     public static void main(String[] args) {
       System.out.println("Hello, Job4j.");
    }
+
 }
