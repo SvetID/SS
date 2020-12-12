aad1ac84fc92811876f11629261b8c431133efdd
Svetlana Dubrova <svetd24@gmail.com>
2020-12-12 12:23:34 +0500

    переменные

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Variables.java
@@ -0,0 +1,14 @@
+package ru.job4j.calculate;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 18;
+        float length = 78.0332f;
+        boolean baby = true;
+        byte number = 127;
+
+        long integer = 2147483647;
+        System.out.println(integer + 1);
+
+    }
+}
